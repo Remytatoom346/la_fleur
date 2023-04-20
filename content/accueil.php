831e6059b2f95285b3d67417fb2de9e04ba596c3
remytatoom346 <remydardennevansse@gmail.com>
2023-04-20 16:27:15 +0200

    accueil

--- content/accueil.php
+++ content/accueil.php
@@ -1,1 +1,62 @@
-<h1>hello world</h1>
+<main class="container_accueil">
+    <section class="section_produit">
+        <div class="tiret_section"></div>
+        <h2>Anniversaire</h2>
+        <ul class="container_produit">
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+        </ul>
+        <a href="anniversaire" class="btn_decouvrir">Découvrir</a>
+    </section>
+    <section class="section_produit">
+        <div class="tiret_section"></div>
+        <h2>Amour & Sentiment</h2>
+        <ul class="container_produit">
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+        </ul>
+        <a href="amour" class="btn_decouvrir">Découvrir</a>
+    </section>
+    <section class="section_produit">
+        <div class="tiret_section"></div>
+        <h2>Mariage</h2>
+        <ul class="container_produit">
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+        </ul>
+        <a href="mariage" class="btn_decouvrir">Découvrir</a>
+    </section>
+    <section class="section_produit">
+        <div class="tiret_section"></div>
+        <h2>Naissance</h2>
+        <ul class="container_produit">
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+        </ul>
+        <a href="naissances" class="btn_decouvrir">Découvrir</a>
+    </section>
+    <section class="section_produit">
+        <div class="tiret_section"></div>
+        <h2>Remerciement</h2>
+        <ul class="container_produit">
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+            <li><img src="" alt="" srcset="ressources/dfi.jpg" class="img_accueil"></li>
+        </ul>
+        <a href="remerciment" class="btn_decouvrir">Découvrir</a>
+    </section>
+</main>
